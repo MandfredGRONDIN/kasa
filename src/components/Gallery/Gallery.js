b115e0e4162da877eae6e7f9a3d5a9c7e63784bb
Mandfred GRONDIN <mandfred974@hotmail.fr>
2022-10-22 23:15:56 +0200

    Update
    Rajout du package react-responsive-carousel, Modification de la partie carousel

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,1 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Products from '../../products.json'
+import '../../styles/test.css'
+
+
+
+function Slider(){
+    const [currImg, setCurrImg] = useState(0)
+    return(
+        <div className='gallery'>
+            {Products.map((product) => (
+                <div key={product.id}>
+                    <Carousel showArrows={true}>
+                        <img src={product.pictures[currImg]} alt=''/>
+                    </Carousel>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Slider
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import '../../styles/Home.css'
 import Img from '../../assets/Home.png'
+import Slider from '../../components/Gallery/Gallery'
 import Products from '../../products.json'
+import { Carousel } from "react-responsive-carousel";
 
 export default function Home(){
+
     return(
         <div className='home'>
             <section>
                 <p>Chez vous, partout et ailleurs</p>
                 <img className='test' src={Img} alt='la nature'></img>
             </section>
-            <div className='gallery'>
-                {Products.map((product) => 
-                <div className='product__item' key={product.id}>
-                    <img src={`${product.pictures[0]}`} alt='logement' className='product__img'></img>
-                    <div className='product__title'>{product.title}</div>
-                </div>)}
-            </div>
+            <Slider/>
         </div>
     )
 }
